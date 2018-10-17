f9f3e3178770febd3c138614cee326a6d5108960
Bregy <bregy.malpartida@utec.edu.pe>
2018-10-16 23:15:28 -0500

    first commit

---
+++ eje.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"encoding/json"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/k0kubun/pp"
+)
+
+const urlPEL = "https://www.peruanosenlinea.com/demo/reniec/example/consulta.php"
+
+func main() {
+	engine := gin.Default()
+
+	engine.Use(cors.Default())
+
+	engine.Any("/getfromessalud", func(c *gin.Context) {
+		dni := struct {
+			Dni string `json:"dni"`
+		}{}
+		c.BindJSON(&dni)
+
+		pp.Println(dni)
+
+		v := url.Values{}
+		v.Set("ndni", dni.Dni)
+		v.Set("source", "EsSalud")
+		resp, err := http.PostForm(urlPEL, v)
+
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		var response interface{}
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			panic(err)
+		}
+		c.JSON(resp.StatusCode, response)
+	})
+
+	engine.Run(":3300")
+}
